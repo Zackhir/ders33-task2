df23ca1f0eb471a92b2f93f56f9ef7dd3e916e1c
Zackhir <qurzakirbdu@gmail.com>
2025-05-06 16:39:15 +0400

    task2 completed

---
+++ src/Components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>Hello
+        <h1>This is header</h1>
+        <p>This is paragraph</p>
+    </div>
+  )
+}
+
+export default Hello
